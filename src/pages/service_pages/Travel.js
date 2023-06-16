953de31e1ff7208e3cdf90c269eb8276e326c8b9
joykara <joykarani1@gmail.com>
2023-06-16 06:33:44 +0300

    updated travel page

--- src/pages/service_pages/Travel.js
+++ src/pages/service_pages/Travel.js
@@ -16,8 +16,8 @@ const Travel = () => {
               <div className="hm-travel-intro">
                   <div className="hm-travel-intro__text">
                       <h4>TRAVEL CONSULTANCY</h4>
-                      <p>Traveling to differing destinations for diverse reasons is an aspiration for many people. Whether for education, touring, or employment, we provide the necessary information and guidance to achieve these objectives. </p>
-                      <p>Hemllin is a destination management company, online travel agency, and visitor information center under the same roof. We understand that many people who want to travel outside the country may not have all the necessary information about the processes and procedures. We fill this gap by providing relevant and reliable information to help our clients prepare, plan, and travel abroad at affordable prices.</p>
+                      <p>Welcome to Hemllin,your trusted partner in creating unforgettable travel experiences. As a premier travel consulting firm, we specialize in providing personalized and tailored services to discerning travelers seeking expert guidance and exceptional journeys.</p>
+                      <p>At Hemllin, we understand that each traveler has unique preferences, interests, and expectations. That's why we take the time to listen and understand your desires, allowing us to curate bespoke travel experiences that surpass your wildest dreams. Our team of experienced travel consultants possesses in-depth knowledge of global destinations, insider connections, and a passion for exploration, ensuring that every aspect of your journey is meticulously planned and executed.</p>
                   </div>
 
                   <div className="hm-travel-content">
@@ -51,111 +51,9 @@ const Travel = () => {
                                     <div className='hm-mng-step-number'>
                                         <h3>2 <span>Strategy Consulting</span></h3>
                                     </div>
-                                    <p>All business success depends on the company's ability to realize its long-term goals. Strategic consulting is one of the most prestigious segments in the industry because it is a professional service that every serious business needs. Since strategy is defined as a plan to realize long-term goals, this service is important in helping companies determine the best approaches to achieve their objectives. </p>
-                                    <p>Strategic consulting targets private-sector clients seeking ways to increase growth and deal with specific and general industry challenges. Unlike other consulting services, strategic consulting is often tied to improving overall economic growth. Given the ongoing economic challenges that affect big organizations as well as small and micro enterprises, strategic consulting has been indispensable. </p>
-                                </div>
-                                <div className="hm-travel-option__content__list">
-                                    <p>The primary services strategic consultants provide include:<br/>
-                                        •	Business model transformation strategy consultation involves evaluating the effectiveness of the current ways of doing business or recommending a model with a higher success probability. <br/>
-                                        •	Corporate strategy consultancy services involve evaluating the internal organizational factors and how the company can improve its performance, productivity, and reputation. <br/>
-                                        •	Mergers and Acquisition consultancy provides information on the best ways to approach business growth or transformation by buying a company or merging with it to increase competitiveness.<br/>
-                                        •	Digital Strategy consultancy services involve determining the best approaches for integrating technology and innovation into business operations to improve efficiency and effectiveness. 
-                                    </p>
-                                    <h4>How do we implement strategic consulting?</h4>
-                                    <p>We begin our consultancy by conducting research to determine the missing gaps and generate relevant solutions. Based on the results generated from the initial investigation, we provide informed guidance on the best strategies to implement and execute them. Strategy consulting services touch on the following areas:</p>
-                                    <ul>
-                                        <li>Budgeting and financial decisions</li>
-                                        <li>Business model transformation</li>
-                                        <li>Corporate decisions</li>
-                                        <li>Digital transformation</li>
-                                        <li>Economic policy</li>
-                                        <li>Functionality</li>
-                                        <li>travel</li>
-                                        <li>Market research</li>
-                                        <li>Organizational structure</li>
-                                        <li>Mergers and acquisitions</li>
-                                        <li>Operations</li>
-                                        <li>Opportunities</li>
-                                        <li>Production</li>
-
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="hm-travel-options-container scale-up">
-                            <div className="hm-travel-option__image">
-                            </div>
-                            <div className="hm-travel-option__content">
-                                <div className="hm-travel-option__content__desc">
-                                    <div className='hm-mng-step-number'>
-                                        <h3>3 <span>Human Resource Consulting</span></h3>
-                                    </div>
-                                    <p>People are the most crucial resource for any organization because they are responsible for fulfilling their job descriptions to meet overall objectives. Human resource consulting involves providing guidance on managing human capital to realize the best organizational outcomes. While many organizations have an idea that individual job descriptions can contribute to the company's success, our human resource consultants will advise on policies and processes that guarantee overall success. </p>
-                                    <p>We offer basic and customized HR consulting services, including human capital strategy, compensations & benefits, organizational change, talent travel, learning and development, and technology. The human capital strategy focuses on defining the corporate culture and setting up the strategy for employees that support the company's key performance pillars. We also advise on how to deal with change in your firm, especially when it is drastic enough to affect productivity and performance.</p>
-                                </div>
-                                <div className="hm-travel-option__content__list">
-                                    <h4>Why Do I Need HR Consulting Services?</h4>
-                                    <p>Hiring employees to perform specific organizational tasks is not enough to guarantee success. You need to know how to hire the right people, to fill the right positions, and filful the right objectives. You need HR consulting services to streamline the various human resources functions and solve the issues affecting employees.</p>
-                                    <p>While the HR department can handle most employee issues, consultants use their expertise, experience, and objective analysis to advise on strategic approaches. Hemllin provides HR consulting in the following areas:</p>
-                                    <ul>
-                                        <li>Human Capital Strategy </li>
-                                        <li>Compensation & benefits </li>
-                                        <li>Organizational change</li>
-                                        <li>HR function</li>
-                                        <li>HR analytics</li>
-                                        <li>Talent travel</li>
-                                        <li>HR technology </li>
-                                        <li>Learning and development</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="hm-travel-options-container scale-up">
-                            <div className="hm-travel-option__image">
-                            </div>
-                            <div className="hm-travel-option__content">
-                                <div className="hm-travel-option__content__desc">
-                                    <div className='hm-mng-step-number'>
-                                        <h3>4 <span>Marketing Consulting</span></h3>
-                                    </div>
-                                    <p>Marketing is not limited to organizations selling products but is at the center of every business endeavor to appeal to customers and relevant stakeholders and achieve its overarching objectives. However, instead of focusing on implementing marketing strategies, consulting focuses on auditing, analyzing, and developing them. </p>
-                                    <p>Marketing consulting involves examining the company's business model, including finances, infrastructure, and customers, to understand how they can be transformed to increase overall marketability. The marketing consultants accomplish this through market research, marketing strategy, analytics, and persona analysis.</p>
-                                    <p>At Hemllin, our consultants help organizations build effective key performance indicators (KPIs) and develop a roadmap for promoting their products and reputation. Marketing consulting enables businesses to access valid and reliable knowledge on the best practice in their industry. We understand that companies hire marketing consultants because they have urgent needs and are prepared to deploy our resources and experience to help them achieve their objectives. </p>
-                                </div>
-                                <div className="hm-travel-option__content__list">
-                                    <h4>Why Do I Need Marketing Consulting Services?</h4>
-                                    <p>The most important aspect of marketing is information and knowledge, which many organizations have neither access to nor the capability to apply in their business endeavors. Consultants have specialized knowledge gathered through years of industry and strategy experience. </p>
-                                    <p>The consultants use this knowledge to help businesses avoid certain mistakes and develop informed strategies to improve the effectiveness of marketing strategies and reach their target audience. External consultants are also more objective because they bring new opinions and strategic tactics. As third-party consultants, we bring a different perspective to tackling marketing problems. </p>
-                                    <p>At Hemllin, we use problem-solving to help our client's travel deal with marketing issues. Our consultants can advise you on different aspects of communicating with the clients based on their needs and wants. 
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="hm-travel-options-container scale-up">
-                            <div className="hm-travel-option__image">
-                            </div>
-                            <div className="hm-travel-option__content">
-                                <div className="hm-travel-option__content__desc">
-                                    <div className='hm-mng-step-number'>
-                                        <h3>5 <span>Financial Advisory</span></h3>
-                                    </div>
-                                    <p>A company's financial well-being is the most important aspect because many businesses are instituted for profitable purposes. Organizations engage in diverse financial transactions that involve different stakeholders, such as suppliers, government, regulators, and customers. For-profit companies engage in transaction services, risk travel, tax advisory, compliance, and litigation services.</p>
-                                    <p>Financial advisory services include transactions services, corporate finance, crisis &recovery, accounting advisory, tax advise, real estate advice, and forensics and litigation. Companies deal with different transactions that include (but are not limited to) divestiture of the firm and dealing with mergers and acquisitions. </p>
-                                    <p>Financial advisory involves corporate finance that deals with funding and capital structures. Since many companies are always looking for capital to scale their business, external consultants provide information on the alternative investment of restructuring proposals to raise the necessary funds. </p>
-                                </div>
-                                <div className="hm-travel-option__content__list">
-                                    <h4>At Hemllin, were are passionate about helping companies deal with their financial difficulties by helping them to turn their fortunes and recover.</h4>
-                                    <p> Our financial experts will help you handle diverse financial difficulties. We consult on issues like bankruptcy or insolvency travel, financial turn-around strategies, risk control, and internal audits. </p>
-                                    <p>Our financial advisory services include:</p>
-                                    <ul>
-                                        <li>Corporate finance</li>
-                                        <li>Transaction services</li>
-                                        <li>Risk travel</li>
-                                        <li>Tax advisory</li>
-                                        <li>Real estate advisory</li>
-                                        <li>Accounting advisory</li>
-                                        <li>Forensic and Litigation</li>
-                                    </ul>
+                                    <p>Whether you're looking for a romantic getaway, an adventurous expedition, a luxurious retreat, or a cultural immersion, we have the expertise to turn your travel aspirations into reality. Our travel consultants are well-versed in crafting customized itineraries that encompass the finest accommodations, exclusive activities, immersive cultural encounters, and seamless logistics. We leave no stone unturned in our pursuit of creating extraordinary moments that will leave you with cherished memories. </p>
+                                  <p>In addition to designing personalized travel experiences, we oer comprehensive travel consulting services. We provide expert advice on destination selection, travel trends, visa requirements, travel insurance, and more. Our consultants stay abreast of the latest developments in the travel industry, ensuring that you receive up-to-date information and guidance throughout your journey. </p>
+                                  <p>What sets us apart is our unwavering commitment to customer satisfaction. We prioritize attention to detail, responsiveness, and personalized service, tailoring every aspect of your travel experience to surpass your expectations. From the moment your each out to us until you return home, we are dedicated to providing you with a seamless and stress-free travel experience</p>
                                 </div>
                             </div>
                         </div>
