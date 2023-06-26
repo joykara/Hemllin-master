c3c0f1a3fde467898a4a561ac8b839ac3c6aa1f7
joykara <joykarani1@gmail.com>
2023-06-26 16:29:15 +0300

    update cookies

--- src/components/cookieConsent/CookieConsent.js
+++ src/components/cookieConsent/CookieConsent.js
@@ -1,37 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useCookies } from 'react-cookie';
 
 const CookieConsent = () => {
-    // setCookies function
-  function setCookies() {
-    document.getElementById('cookiesblock').style.display='none';
-    localStorage.setItem('cookies', 'true');
-  }
   const [cookies, setCookie] = useCookies(["cookieConsent"]);
+
+  useEffect(() => {
+    const consentCookie = cookies['cookieConsent'];
+
+    if (consentCookie) {
+      // User has given consent, hide the cookie consent block
+      document.getElementById('cookiesblock').style.display = 'none';
+    }
+  }, [cookies]);
+
   const giveCookieConsent = () => {
-    setCookie("cookieConsent", true, { path: "/" });
+    setCookie("cookieConsent", true, { path: "/about-us" });
+  };
+
+  const setCookies = () => {
+    document.getElementById('cookiesblock').style.display = 'none';
   };
 
   return (
     <>
-        <div class="cookiesblock" id="cookiesblock">
-            <div class="cookies-container">
-            <div class="row align-items-center">
-                <div class="col-dets">
-                <p>This website uses cookies to ensure you get the best experience on our website. Read our&nbsp;<span><a href={"/privacy-policy"}>Cookie Policy</a></span>&nbsp;to learn more</p><p></p>
-                </div>
-                <div class="col-dets">
-                <div class="cookiesbtn">
-                    <ul>
-                        <li><button onClick={giveCookieConsent} data-button="true" aria-label="Accept usage of cookie">Accept</button></li>
-                        <li><button onClick={setCookies} aria-label="Dismiss usage of cookie">Dismiss</button></li>
-                    </ul>
-                </div>
-                </div>
+      <div className="cookiesblock" id="cookiesblock">
+        <div className="cookies-container">
+          <div className="row align-items-center">
+            <div className="col-dets">
+              <p>
+                This website uses cookies to ensure you get the best experience on our website. Read our&nbsp;
+                <span><a href={"/privacy-policy"}>Cookie Policy</a></span>&nbsp;to learn more
+              </p>
+              <p></p>
             </div>
+            <div className="col-dets">
+              <div className="cookiesbtn">
+                <ul>
+                  {!cookies['cookieConsent'] && (
+                    <li>
+                      <button onClick={giveCookieConsent} data-button="true" aria-label="Accept usage of cookie">
+                        Accept
+                      </button>
+                    </li>
+                  )}
+                  {!cookies['cookieConsent'] && (
+                    <li>
+                      <button onClick={setCookies} data-button="true" aria-label="Dismiss usage of cookie">
+                        Dismiss
+                      </button>
+                    </li>
+                  )}
+                </ul>
+              </div>
             </div>
+          </div>
         </div>
-    
+      </div>
     </>
   );
 };
