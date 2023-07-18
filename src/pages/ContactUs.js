9e8082f32629b62008163ab061de2081f0ad3f68
joykara <joykarani1@gmail.com>
2023-07-18 12:26:20 +0300

    update

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -13,7 +13,7 @@ const ContactUs = () => {
     e.preventDefault();
 
     // Send the contact form to the server using axios.post
-    axios.post('https://hemllin-master.vercel.app/contact-us/data', {
+    axios.post('/contact-us/data', {
       name: form.current.username.value,
       email: form.current.email.value,
       organization: form.current.organization.value,
