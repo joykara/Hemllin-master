fccfc35262069324abcddf04acef8890242cbf2d
joykara <joykarani1@gmail.com>
2023-07-22 10:57:00 +0300

    update

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -4,50 +4,50 @@ import List from '../../data/List';
 import './searchbar.css';
 
 const SearchBar = () => {
-    // SEARCH FUNCTIONALITY
-    const [state, dispatch] = useReducer((state, action) => {
-      switch (action.type) {
-        case 'inputText':
-          return {
-          ...state,
-            inputText: action.payload,
-          };
-        case'showData':
-          return {
-          ...state,
-            showData: action.payload,
-          };
-        default:
-          return state;
-      }
-    }, {
-      inputText: '',
-      showData: '',
-    });
-  
-    const { inputText, showData } = state;
-  
-    const handleInputText = (e) => {
-      dispatch({ type: 'inputText', payload: e.target.value });
-    };
-    
-    const handleShowData = (e) => {
-      dispatch({ type: 'showData', payload: e.target.value });
-    };
-    
-    let inputHandler = (e) => {
-      var lowerCase = e.target.value.toLowerCase();
-      dispatch({ type: 'inputText', payload: lowerCase });
-    };
-    // SEARCHBAR DISPLAY FUNCTION
-    const [showSearchBar, setShowSearchBar] = useState(false);
-    const handleShowSearchBar = () => {
-      setShowSearchBar(showSearchBar);
-    };
+  // SEARCH FUNCTIONALITY
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case 'inputText':
+        return {
+        ...state,
+          inputText: action.payload,
+        };
+      case'showData':
+        return {
+        ...state,
+          showData: action.payload,
+        };
+      default:
+        return state;
+    }
+  }, {
+    inputText: '',
+    showData: '',
+  });
+
+  const { inputText, showData } = state;
+
+  const handleInputText = (e) => {
+    dispatch({ type: 'inputText', payload: e.target.value });
+  };
+
+  const handleShowData = (e) => {
+    dispatch({ type: 'showData', payload: e.target.value });
+  };
+
+  let inputHandler = (e) => {
+    var lowerCase = e.target.value.toLowerCase();
+    dispatch({ type: 'inputText', payload: lowerCase });
+  };
+
+  // SEARCHBAR DISPLAY FUNCTION
+  const [showSearchBar, setShowSearchBar] = useState(false);
+  const handleShowSearchBar = () => {
+    setShowSearchBar(showSearchBar);
+  };
 
   return (
     <>
-      
         <div className="navbar__menu-links__searchbar">
           <input name='search' type="text" onChange={inputHandler} onClick={handleInputText} onKeyUp={handleShowData} placeholder="Type to search..." />
           <button onClick={handleShowSearchBar}><RiCloseLine size={45} /></button>
